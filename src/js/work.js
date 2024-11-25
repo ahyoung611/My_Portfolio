fe16beefc5b53dc013485e9dbd1e85604c075a5d
RumbleRum <dnjswls0339@naver.com>
2024-11-25 16:08:56 +0900

    06.uxui 추가

---
+++ src/js/work.js
@@ -0,0 +1,30 @@
+export function work() {
+    var workSwiper = new Swiper(".workSwiper", {
+       
+        slidesPerView: 1,
+        spaceBetween:50,
+        centeredSlides: true,
+        loop: true, // 루프 비활성화
+
+        breakpoints: {
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 92,
+                centeredSlides: true,
+                initialSlide: 2, // 첫 로드 시 가운데 슬라이드 (3번 슬라이드가 중앙)
+
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 92,
+                centeredSlides: true,
+                initialSlide: 2,
+            },
+        },
+        navigation: {
+            nextEl: "#wrokSwiper-box .swiper-button-next",
+            prevEl: "#wrokSwiper-box .swiper-button-prev",
+          },
+    });
+
+}
