d8751f5f5dcfc23d6f4e675bddded161e7427f63
RumbleRum <dnjswls0339@naver.com>
2024-11-25 16:06:37 +0900

    05.work 추가

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 import { menu } from "./src/js/menu";
 import { intro } from "./src/js/intro";
 import { project } from "./src/js/project";
+import { work } from "./src/js/work";
 
 window.addEventListener('load',function(){
     menu()
     intro()
     project()
+    work()
 })
